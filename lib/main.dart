1c561b0bfa4d6a13dd80426b75553ded27a1f916
peymansf2000 <peymansf2000@outlook.com>
2024-01-23 21:04:19 +0330

    Add ApiProvider for current weather API
    Implemented ApiProvider class to handle API requests for the weather feature. This commit includes calling the OpenWeather API for current weather data.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'core/widgets/main_wrapper.dart';
+
 void main() {
   runApp(const MainApp());
 }
@@ -9,12 +11,6 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+    return const MaterialApp(home: MainWrapper());
   }
 }
