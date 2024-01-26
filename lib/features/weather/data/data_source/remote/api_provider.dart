ee3399f034c389c2c4f81a1d90079ef51d9e99d2
peymansf2000 <peymansf2000@outlook.com>
2024-01-26 22:52:55 +0330

    Update ApiProvider of current weather API

--- lib/features/weather/data/data_source/remote/api_provider.dart
+++ lib/features/weather/data/data_source/remote/api_provider.dart
@@ -1,1 +1,16 @@
+import 'package:dio/dio.dart';
+import 'package:skylite/core/utils/constants.dart';
 
+class ApiProvider {
+  final Dio _dio = Dio();
+
+  var apiKey = Constants.openWeather['apiKey'];
+
+  /// current weather api
+  Future<dynamic> callCurrentWeather(cityName) async {
+    var response = await _dio.get(
+        "${Constants.openWeather['baseUrl']}/data/2.5/weather",
+        queryParameters: {'q': cityName, 'appid': apiKey, 'units': 'metric'});
+    return response;
+  }
+}
