c9a4668ff05ef187f11ebd3c422d77ca12869b13
peymansf2000 <peymansf2000@outlook.com>
2024-02-01 00:07:07 +0330

    Update access modifiers
    Update access modifiers of get_current_weather usecase

--- lib/features/weather/domain/usecases/get_current_weather_usecase.dart
+++ lib/features/weather/domain/usecases/get_current_weather_usecase.dart
@@ -1,15 +1,15 @@
 import 'package:skylite/core/resources/data_state.dart';
 import 'package:skylite/core/usecases/usecase.dart';
-import 'package:skylite/features/weather/data/repository/weather_repository.dart';
 import 'package:skylite/features/weather/domain/entities/current_city_entity.dart';
+import 'package:skylite/features/weather/domain/repository/weather_repository.dart';
 
 class GetCurrentWeatherUsecase
     implements Usecase<DataState<CurrentCityEntity>, String> {
-  final WeatherRepository weatherRepository;
-  GetCurrentWeatherUsecase(this.weatherRepository);
+  final IWeatherRepository _weatherRepository;
+  GetCurrentWeatherUsecase(this._weatherRepository);
 
   @override
   Future<DataState<CurrentCityEntity>> call(String cityName) {
-    return weatherRepository.fetchCurrentWeatherData(cityName);
+    return _weatherRepository.fetchCurrentWeatherData(cityName);
   }
 }
