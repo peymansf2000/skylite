bf0f1eb55df60d811c0a01702a8c03c6aee2c40d
peymansf2000 <peymansf2000@outlook.com>
2024-02-01 00:09:49 +0330

    Add different states of Home page

---
+++ lib/features/weather/presentation/bloc/home_state.dart
@@ -0,0 +1,22 @@
+part of 'home_bloc.dart';
+
+sealed class HomeState extends Equatable {
+  const HomeState();
+
+  @override
+  List<Object> get props => [];
+}
+
+final class HomeError extends HomeState {
+  final String message;
+
+  const HomeError({required this.message});
+}
+
+final class HomeCompleted extends HomeState {
+  final CurrentCityEntity currentCityEntity;
+
+  const HomeCompleted({required this.currentCityEntity});
+}
+
+final class HomeLoading extends HomeState {}
