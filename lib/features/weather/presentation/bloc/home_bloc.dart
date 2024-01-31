1cdb007a5e68826ed8173d9750cf4baa92ba863d
peymansf2000 <peymansf2000@outlook.com>
2024-02-01 00:11:28 +0330

    Add home_bloc

---
+++ lib/features/weather/presentation/bloc/home_bloc.dart
@@ -0,0 +1,25 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:skylite/core/resources/data_state.dart';
+import 'package:skylite/features/weather/domain/entities/current_city_entity.dart';
+import 'package:skylite/features/weather/domain/usecases/get_current_weather_usecase.dart';
+
+part 'home_event.dart';
+part 'home_state.dart';
+
+class HomeBloc extends Bloc<HomeEvent, HomeState> {
+  final GetCurrentWeatherUsecase getCurrentWeatherUsecase;
+  HomeBloc(this.getCurrentWeatherUsecase) : super(HomeLoading()) {
+    on<HomeStarted>((event, emit) async {
+      emit(HomeLoading());
+      DataState<CurrentCityEntity> dataState =
+          await getCurrentWeatherUsecase(event.cityName);
+      switch (dataState.runtimeType) {
+        case (DataSuccess):
+          emit(HomeCompleted(currentCityEntity: dataState.data!));
+        case (DataFailed):
+          emit(HomeError(message: dataState.error!));
+      }
+    });
+  }
+}
