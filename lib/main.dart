67fbcc08365ba3ba18574c5b971eadc946dba8cd
peymansf2000 <peymansf2000@outlook.com>
2024-02-02 22:23:10 +0330

    Add setup for statusbar and systemnavbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:skylite/features/weather/presentation/bloc/home_bloc.dart';
 import 'package:skylite/locator.dart';
 
 import 'core/widgets/main_wrapper.dart';
 
 void main() async {
   await setup();
-  runApp(const MainApp());
+  runApp(
+    const MyApp(),
+  );
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: MainWrapper());
+    // Set the status bar color dynamically based on the theme primary color
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+        statusBarColor: Theme.of(context).primaryColor,
+        statusBarIconBrightness: Brightness.dark,
+        systemNavigationBarIconBrightness: Brightness.dark,
+        systemNavigationBarDividerColor: Theme.of(context).primaryColor,
+        systemNavigationBarColor: Theme.of(context).primaryColor));
+
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: MultiBlocProvider(
+            providers: [BlocProvider(create: (_) => locator<HomeBloc>())],
+            child: MainWrapper()));
   }
 }
