013e3c234708d41a4759ee748deb28c916c0a267
peymansf2000 <peymansf2000@outlook.com>
2024-02-02 22:26:38 +0330

    Add mainWrapper

--- lib/core/widgets/main_wrapper.dart
+++ lib/core/widgets/main_wrapper.dart
@@ -1,13 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:skylite/core/widgets/bottom_nav.dart';
+import 'package:skylite/features/bookmark/presentation/screens/bookmark_screen.dart';
+import 'package:skylite/features/weather/presentation/screens/home_screen.dart';
 
 class MainWrapper extends StatelessWidget {
-  const MainWrapper({super.key});
+  MainWrapper({super.key});
 
+  final PageController _controller = PageController(initialPage: 0);
+  final List<Widget> screens = const [HomeScreen(), BookmarkScreen()];
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Container(),
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+              image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage('assets/images/dark_background.jpg'))),
+          child: PageView(
+            controller: _controller,
+            children: screens,
+          ),
+        ),
+        bottomNavigationBar: BottomNav(controller: _controller),
+      ),
     );
   }
 }
