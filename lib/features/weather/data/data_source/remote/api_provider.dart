c8e155cd0f44477e3e05d1f98958480e439fb4b5
peymansf2000 <peymansf2000@outlook.com>
2024-02-06 22:08:23 +0330

    Add CallForecastWeather to apiProvider

--- lib/features/weather/data/data_source/remote/api_provider.dart
+++ lib/features/weather/data/data_source/remote/api_provider.dart
@@ -1,16 +1,29 @@
 import 'package:dio/dio.dart';
+import 'package:skylite/core/params/forecast_params.dart';
 import 'package:skylite/core/utils/constants.dart';
+import 'package:skylite/features/weather/domain/entities/current_city_entity.dart';
+
+var apiKey = Constants.openWeather['apiKey'];
 
 class ApiProvider {
   final Dio _dio = Dio();
-
-  var apiKey = Constants.openWeather['apiKey'];
 
   /// current weather api
   Future<dynamic> callCurrentWeather(cityName) async {
     var response = await _dio.get(
         "${Constants.openWeather['baseUrl']}/data/2.5/weather",
         queryParameters: {'q': cityName, 'appid': apiKey, 'units': 'metric'});
+    return response;
+  }
+
+  Future<dynamic> callForecastWeather(ForecastParams forecastParams) async {
+    var response = await _dio.get(
+        "${Constants.openWeather['baseUrl']}/data/2.5/forecast/daily",
+        queryParameters: {
+          'lat': forecastParams.lat,
+          'lon': forecastParams.lon,
+          'appid': apiKey
+        });
     return response;
   }
 }
